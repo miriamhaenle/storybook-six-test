d032bc4cab9f888597209ca957f8295e411e9812
Miriam <miriam.haenle@gmail.com>
2020-09-06 12:45:53 +0200

    Write story for navigation

---
+++ stories/2-Navigation.stories.js
@@ -0,0 +1,47 @@
+export default {
+  title: 'components/Navigation',
+  argTypes: {
+    children: { control: 'text' },
+  },
+}
+
+const Template = ({ children, backgrounds }) => {
+  const navContainer = document.createElement('section')
+  navContainer.classList.add('sidebar')
+  const nav = document.createElement('nav')
+  nav.innerHTML = children
+  navContainer.appendChild(nav)
+  return navContainer
+}
+
+export const NavigationHomeActive = Template.bind({})
+NavigationHomeActive.args = {
+  children: `<a href="" class="nav-item active">Home</a>
+  <a href="" class="nav-item">About</a>
+  <a href="" class="nav-item ">Portfolio</a>
+  <a href="" class="nav-item">Contact</a>`,
+}
+
+export const NavigationAboutActive = Template.bind({})
+NavigationAboutActive.args = {
+  children: `<a href="" class="nav-item ">Home</a>
+  <a href="" class="nav-item active">About</a>
+  <a href="" class="nav-item ">Portfolio</a>
+  <a href="" class="nav-item">Contact</a>`,
+}
+
+export const NavigationPortfolioActive = Template.bind({})
+NavigationPortfolioActive.args = {
+  children: `<a href="" class="nav-item ">Home</a>
+  <a href="" class="nav-item ">About</a>
+  <a href="" class="nav-item active ">Portfolio</a>
+  <a href="" class="nav-item">Contact</a>`,
+}
+
+export const NavigationContactActive = Template.bind({})
+NavigationContactActive.args = {
+  children: `<a href="" class="nav-item ">Home</a>
+  <a href="" class="nav-item ">About</a>
+  <a href="" class="nav-item ">Portfolio</a>
+  <a href="" class="nav-item active">Contact</a>`,
+}
---
+++ stories/3-Portfolio.stories.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Portfolio',
+  argTypes: {
+    children,
+  },
+}
